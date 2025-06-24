3e7d5696c33a7e94bca740207ff77c7b58d03e73
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-06-24 18:48:37 -0300

    Exercício: Operadores de Atribuição em Java

---
+++ OperadoresAtribuicão.java
@@ -0,0 +1,13 @@
+// Neste exercício, você usará diferentes operadores de atribuição para manipular o valor de uma variável inteira.
+
+public class OperadoresAtribuicão {
+    public static void main(String[] args) {
+        var numero = 10;
+        numero += 3;
+        numero -= 2;
+        numero *= 4;
+        numero /= 2;
+
+        System.out.println(numero);
+    }
+}
