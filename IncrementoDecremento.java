49ff1435ba0fc0f2e2cacaf1ecb49c39a6398334
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-06-24 18:57:21 -0300

    Incremento e Decremento em Java

---
+++ IncrementoDecremento.java
@@ -0,0 +1,11 @@
+// Praticar o uso dos operadores de incremento (++) e decremento (--) em Java.
+
+public class IncrementoDecremento {
+    public static void main(String[] args) {
+        var contador = 5;
+        contador++;
+        contador--;
+
+        System.out.println(contador);
+    }
+}
