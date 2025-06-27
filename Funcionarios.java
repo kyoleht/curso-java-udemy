d2d4acc74d158279b19f42b42d1c5383b8c7dda9
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-06-27 12:08:33 -0300

    Adicionando um segundo funcionário

--- Funcionarios.java
+++ Funcionarios.java
@@ -6,14 +6,26 @@ public class Funcionarios {
     int idade;
 
     public static void main(String[] args) {
-        Funcionarios funcionario = new Funcionarios();
+        Funcionarios funcionario1 = new Funcionarios();
 
-        funcionario.nome = "Rafael";
-        funcionario.cargo = "Desenvolvedor";
-        funcionario.idade = 20;
+        funcionario1.nome = "Rafael";
+        funcionario1.cargo = "Desenvolvedor";
+        funcionario1.idade = 20;
 
-        System.out.println("Nome: " + funcionario.nome);
-        System.out.println("Cargo: " + funcionario.cargo);
-        System.out.println("Idade: " + funcionario.idade);
+        System.out.println("Nome: " + funcionario1.nome);
+        System.out.println("Cargo: " + funcionario1.cargo);
+        System.out.println("Idade: " + funcionario1.idade);
+        
+        // adicionando um segundo funcionário
+        Funcionarios funcionario2 = new Funcionarios();
+
+        funcionario2.nome = "Jessica";
+        funcionario2.cargo = "Social Media";
+        funcionario2.idade = 21;
+
+
+        System.out.println("Nome: " + funcionario2.nome);
+        System.out.println("Cargo: " + funcionario2.cargo);
+        System.out.println("Idade: " + funcionario2.idade);
     }
 } 
