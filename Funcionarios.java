72951e094315bb87605a09bbe85952ff5118a172
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-06-28 18:04:46 -0300

    Utilizando this. na POO

--- Funcionarios.java
+++ Funcionarios.java
@@ -5,27 +5,22 @@ public class Funcionarios {
     String cargo;
     int idade;
 
+    Funcionarios(String nome, String cargo, int idade) {
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+    }
+
     public static void main(String[] args) {
-        Funcionarios funcionario1 = new Funcionarios();
+        Funcionarios funcionario1 = new Funcionarios("Rafael", "Desenvolvedor", 20);
+        Funcionarios funcionario2 = new Funcionarios("Jessica", "Social Media", 21);
 
-        funcionario1.nome = "Rafael";
-        funcionario1.cargo = "Desenvolvedor";
-        funcionario1.idade = 20;
-
-        System.out.println("Nome: " + funcionario1.nome);
-        System.out.println("Cargo: " + funcionario1.cargo);
-        System.out.println("Idade: " + funcionario1.idade);
-        
-        // adicionando um segundo funcionário
-        Funcionarios funcionario2 = new Funcionarios();
-
-        funcionario2.nome = "Jessica";
-        funcionario2.cargo = "Social Media";
-        funcionario2.idade = 21;
-
-
-        System.out.println("Nome: " + funcionario2.nome);
-        System.out.println("Cargo: " + funcionario2.cargo);
-        System.out.println("Idade: " + funcionario2.idade);
+        System.out.println(funcionario1.nome);
+        System.out.println(funcionario1.cargo);
+        System.out.println(funcionario1.idade);
+        System.out.println("-----------------");
+        System.out.println(funcionario2.nome);
+        System.out.println(funcionario2.cargo);
+        System.out.println(funcionario2.idade);
     }
 } 
