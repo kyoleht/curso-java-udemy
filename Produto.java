5a4475da79668276dbdea978e7aab0f63a68ae86
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-07-04 05:12:14 -0300

    Refatorando meu código

--- Produto.java
+++ Produto.java
@@ -7,25 +7,32 @@ public class Produto {
 
     public Produto(String nome, double preco, int quantidadeEmEstoque) {
         if (preco < 0) {
-            throw new IllegalArgumentException("Preço não pode ser negativo"); // programa é interrompido com mensagem de erro
+            throw new IllegalArgumentException("ERRO: Preço não pode ser negativo!"); // programa é interrompido com mensagem de erro
         }
-        
+        if (quantidadeEmEstoque < 0) {
+            throw new IllegalArgumentException("ERRO: Estoque não pode ser negativo!"); 
+        }
+
         this.nome = nome;
         this.preco = preco;
         this.quantidadeEmEstoque = quantidadeEmEstoque;
     }
 
-    public void exibirLoja() {
-        System.out.println("Item: " + nome + " | " + "Preço: " + preco + " | " + "Em estoque: " + quantidadeEmEstoque);
+    public String obterInfo() { // mudei de "class" para "String" para poder o reutilizá-lo melhor
+        return "Item: " + nome + " | " + "Preço: " + preco + " | " + "Em estoque: " + quantidadeEmEstoque; // return na classe Main
     }
 
     // acrescentado para futuramente criar um carrinho de compra
-    public void vender(int carrinho) {
-        if (carrinho <= quantidadeEmEstoque) {
-            quantidadeEmEstoque -= carrinho;
-            System.out.println(carrinho + " itens adicionado ao carrinho.");
+    public void vender(int quantidadeVendida) {
+        if (quantidadeVendida <= 0) {
+            throw new IllegalArgumentException("ERRO: Quantidade inválida para venda!");
+        }
+
+        if (quantidadeVendida <= quantidadeEmEstoque) {
+            quantidadeEmEstoque -= quantidadeVendida;
+            System.out.println(quantidadeVendida + " itens adicionado ao carrinho.");
         } else {
-            System.out.println("Não há tantos itens em estoque");
+            throw new IllegalArgumentException("ERRO: Não há tantos(as) " + nome + "(s) em estoque."); // utilizando a nova função no false
         }
     }
 }
@@ -34,9 +41,13 @@ public class Produto {
     public static void main(String[] args) {
             Produto product1 = new Produto("Espada", 350, 15);
             Produto product2 = new Produto("Armadura de Ferro", 600, 10);
-            Produto invalidProduct = new Produto("Poção de Cura", -90, 3);
+            Produto invalid1 = new Produto("Capsula de Força", -90, -3);
+            
 
-            invalidProduct.exibirLoja(); // preço setado em 0.0;
+            System.out.println(product1.obterInfo());
+            product1.vender(5);
+            System.out.println(product1.obterInfo());
+            // Está exibindo a mensagem de erro da instância 'invalid1', mesmo eu não a chamando. Preciso arrumar isso depois; 
     }
 }
 
