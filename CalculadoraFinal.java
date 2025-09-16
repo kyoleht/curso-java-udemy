7ec74c6b7c2bfc0ba2352e7ac853bfb7707cdfdf
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-09-15 22:27:25 -0300

    Adiciona a classe CalculadoraFinal com um método final para soma e uma classe interna para calculadora científica

---
+++ CalculadoraFinal.java
@@ -0,0 +1,12 @@
+/* public class CalculadoraFinal {
+    int a;
+    int b;
+
+    final void somar(int a, int b) {
+        int soma = a + b;
+    }
+
+    class CalculadoraCientifica extends CalculadoraFinal {
+        // CalculadoraFinal.somar(); -> erro: Cannot override the final method from CalculadoraFinal
+    }
+} */
