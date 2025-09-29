a1608ead164fcb9ea97b66c6247dbded48c07839
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-09-28 23:36:32 -0300

    Refatora a interface Product e a classe ProdutoImpl para corrigir a implementação do método infoProduto e melhorar a manipulação de quantidades

--- ProdutoImpl.java
+++ ProdutoImpl.java
@@ -1,19 +1,53 @@
 interface Product {
-    void infoProduto(String item, int quantidade);
+    void infoProduto();
 }
 
 public class ProdutoImpl implements Product {
+    public String item;
+    private int quantidade;
+
+    public ProdutoImpl(String item, int quantidade) {
+        this.item = item;
+        this.quantidade = quantidade;
+    }
+
     @Override
-    public void infoProduto(String item, int quantidade) {
+    public void infoProduto() {
         System.out.println("Item: " + item + ", Quantidade: " + quantidade);
     }
-    public static void main(String[] args) {
-        ProdutoImpl item1 = new ProdutoImpl();
-        ProdutoImpl item2 = new ProdutoImpl();
-        ProdutoImpl item3 = new ProdutoImpl();
-    
-        item1.infoProduto("Computador Gamer", 12); 
-        item2.infoProduto("Notebook", 8);
-        item3.infoProduto("Cafeteira", 6);
+
+    public class adicionarQuantidade {
+        public int quantidade;
+        public void adicionar(int quantidadeAtualizada) {
+            quantidadeAtualizada += quantidade;
+            System.out.println("Quantidade atualizada: " + quantidadeAtualizada);
+        }
     }
-}
+
+    public void removerQuantidade(int quantidadeRemover) {
+        if (quantidade >= quantidadeRemover) {
+            quantidade -= quantidadeRemover;
+            System.out.println("Quantidade vendida: " + quantidadeRemover);
+        } else {
+            System.out.println("Quantidade insuficiente para remover.");
+        }
+    }
+
+    public static void main(String[] args) {
+            ProdutoImpl item1 = new ProdutoImpl("Computador", 10);
+            ProdutoImpl item2 = new ProdutoImpl("Notebook", 12);
+            ProdutoImpl item3 = new ProdutoImpl("Cafeteira", 15);
+
+            item1.infoProduto();
+            item1.removerQuantidade(3);
+            item1.infoProduto();
+
+            item2.infoProduto();
+            item2.removerQuantidade(9);
+            item2.infoProduto();
+
+            item3.infoProduto();
+            item3.removerQuantidade(5);
+            item3.infoProduto();
+        }
+    }
