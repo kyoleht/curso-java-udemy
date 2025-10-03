60955f07fa89d14107f8168a80fd452de419dd5b
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-10-02 23:12:01 -0300

    adiciona a classe Loja e a classe LojaComercial com métodos para abrir e fechar a loja

---
+++ LojaComercial.java
@@ -0,0 +1,35 @@
+
+abstract class Loja {
+
+    private String cnpj;
+    private String razaoSocial;
+    protected boolean aberta;
+
+    public void abrir() {
+        this.aberta = true;
+    }
+
+    public void fechar() {
+        this.aberta = false;
+    }
+
+    public Loja(String cnpj, String razaoSocial) {
+        this.cnpj = cnpj;
+        this.razaoSocial = razaoSocial;
+        this.aberta = false;
+    }
+}
+
+class LojaComercial extends Loja {
+    public LojaComercial(String cnpj, String razaoSocial) {
+        super(cnpj, razaoSocial);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LojaComercial minhaLoja = new LojaComercial("123456789", "Minha Loja");
+        minhaLoja.abrir();
+        System.out.println("A loja está aberta? " + minhaLoja.aberta); 
+    }
+}
