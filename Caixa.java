3909b915f92fdd57de71c64457e0483cafd1324c
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-10-07 23:11:00 -0300

    refatora a classe Caixa para utilizar um array de elementos genéricos, removendo métodos obsoletos e ajustando o main para testes

--- Caixa.java
+++ Caixa.java
@@ -1,21 +1,36 @@
 public class Caixa<T> {
-    private T coisaNaCaixa;
+    private T[] elementos;
 
-    public void guardar(T coisa) {
-        this.coisaNaCaixa = coisa;
+    public Caixa(int capacidade) {
+        elementos = (T[]) new Object[capacidade];
     }
 
-    public T pegar() {
-        return coisaNaCaixa;
+    public void adicionar(T elemento, int indice) {
+        elementos[indice] = elemento;
+    }
+
+    public T obter(int indice) {
+        return elementos[indice];
     }
 
     public static void main(String[] args) {
-        Caixa<String> caixaDeStrings = new Caixa<>();
-        caixaDeStrings.guardar("Olá, Mundo!");
-        System.out.println(caixaDeStrings.pegar());
+        // Criando uma CaixaDePreços para Double
+        Caixa<Double> caixaDePrecos = new Caixa<>(3);
 
-        Caixa<Integer> caixaDeNumeros = new Caixa<>();
-        caixaDeNumeros.guardar(123);
-        System.out.println(caixaDeNumeros.pegar());
+        caixaDePrecos.adicionar(10.5, 0);
+        caixaDePrecos.adicionar(20.0, 1);
+
+        System.out.println("Preço 1: " + caixaDePrecos.obter(0)); // Saída: 10.5
+        System.out.println("Preço 2: " + caixaDePrecos.obter(1)); // Saída: 20.0
+
+        // Criando uma CaixaDeLetras para Character
+        Caixa<Character> caixaDeLetras = new Caixa<>(2);
+
+        caixaDeLetras.adicionar('A', 0);
+        caixaDeLetras.adicionar('B', 1);
+        
+        System.out.println("Letra 1: " + caixaDeLetras.obter(0)); // Saída: A
+        System.out.println("Letra 2: " + caixaDeLetras.obter(1)); // Saída: B
     }
 }
+
