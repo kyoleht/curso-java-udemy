345b455e2d74f5fabd96236fd1632f713f503285
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-10-19 23:12:26 -0300

    adiciona exemplo de escrita e leitura de arquivos utilizando NIO e cria o arquivo anotacoes.txt

--- ExemploNIO.java
+++ ExemploNIO.java
@@ -1,10 +1,9 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-
 public class ExemploNIO {
     public static void main(String[] args) {
-        Path path = Paths.get("Arquivo.txt");
+        /* Path path = Paths.get("Arquivo.txt");
         try {
             // Escrevendo em um arquivo
             Files.write(path, "Olá, Mundo NIO!".getBytes());
@@ -16,6 +15,23 @@ public class ExemploNIO {
 
         } catch (Exception e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
+        } */
+
+        Path newPath = Paths.get("anotacoes.txt");
+        try {
+            Files.write(newPath, "Hoje, aprendi sobre NIO!".getBytes());
+        } catch (Exception e) {
+            System.out.println("Erro ao ler o newPath: " + e.getMessage());
+        } 
+
+        try {
+            if (Files.exists(newPath)) {
+                byte[] bytes = Files.readAllBytes(newPath);
+                String content = new String(bytes);
+                System.out.println("Arquivo criado com sucesso! - " + content);
+            }
+        } catch (Exception e) {
+            System.out.println("Arquivo não encontrado! Erro: " + e.getMessage());
         }
     }
 }
