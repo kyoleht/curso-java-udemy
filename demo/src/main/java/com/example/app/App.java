e63b084c2955a6ffdbbb8abb8d001a40a828af57
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-10-26 05:54:04 -0300

    trabalhando com Pos

--- demo/src/main/java/com/example/app/App.java
+++ demo/src/main/java/com/example/app/App.java
@@ -1,23 +1,34 @@
 package com.example.app;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
 
 public class App extends Application {
     @Override
     public void start(Stage palco) {
-        Label label = new Label("Digite seu nome: ");
-        TextField campoTexto = new TextField();
-        Button botao = new Button("Concluir");
-        VBox layout = new VBox(label, campoTexto, botao);
+        Button botao1 = new Button("Botão 1");
+        Button botao2 = new Button("Botão 2");
+        Button botao3 = new Button("Botão 3");
+        Button botao4 = new Button("Botão 4");
 
-        Scene cena = new Scene(layout, 300, 200);
-        
+        HBox hbox = new HBox(botao1, botao2);
+        hbox.setAlignment(Pos.CENTER);
+
+        VBox vbox = new VBox(botao3, botao4);
+        vbox.setAlignment(Pos.CENTER);
+
+        BorderPane borderpane = new BorderPane();
+        borderpane.setTop(hbox);
+        borderpane.setCenter(vbox);
+
+        Scene cena = new Scene(borderpane, 300, 300);
         palco.setScene(cena);
         palco.show();
     }
