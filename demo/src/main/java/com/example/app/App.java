5557f2ba2bee091a8ab8be4d8f0e45ea7ebb88ef
kyoleht <rafaelferreiradasilva1812@gmail.com>
2025-11-04 20:15:34 -0300

    Utilização ed FileChooser com JavaFX

--- demo/src/main/java/com/example/app/App.java
+++ demo/src/main/java/com/example/app/App.java
@@ -1,38 +1,56 @@
 package com.example.app;
 
+import java.io.File;
+
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-
 public class App extends Application {
+    
     @Override
     public void start(Stage palco) {
-        Button botao1 = new Button("Botão 1");
-        Button botao2 = new Button("Botão 2");
-        Button botao3 = new Button("Botão 3");
-        Button botao4 = new Button("Botão 4");
+        FileChooser selector = new FileChooser();
+        Button openButton = new Button("Abrir arquivo");
+        
+        openButton.setOnAction(e -> {
+            File selectedFile = selector.showOpenDialog(palco);
+            if(selectedFile != null) {
+                selectedImage(selectedFile, palco);
+            }
+        });
 
-        HBox hbox = new HBox(botao1, botao2);
-        hbox.setAlignment(Pos.CENTER);
-
-        VBox vbox = new VBox(botao3, botao4);
-        vbox.setAlignment(Pos.CENTER);
-
-        BorderPane borderpane = new BorderPane();
-        borderpane.setTop(hbox);
-        borderpane.setCenter(vbox);
-
-        Scene cena = new Scene(borderpane, 300, 300);
+        VBox layout = new VBox(openButton);
+        Scene cena = new Scene(layout, 800, 800);
+        palco.setTitle("Testando FileChooser");
         palco.setScene(cena);
         palco.show();
     }
 
+    private void selectedImage(File file, Stage palcoSelectedFile) {
+        Image image = new Image(file.toURI().toString());
+        ImageView imageView = new ImageView(image);
+
+        VBox layout;
+
+        //obtendo o layout atual
+        layout = (VBox) palcoSelectedFile.getScene().getRoot();
+
+        //adicionando imagem ao layout 
+        layout.getChildren().add(imageView);
+
+        //ajustando tamanho da imagem
+        imageView.setFitHeight(500);
+        imageView.setFitWidth(500);
+
+        palcoSelectedFile.setTitle("File Selector");
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
